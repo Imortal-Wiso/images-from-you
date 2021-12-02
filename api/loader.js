935aecfe25b663658ae0ccbb0268fb31fb365833
Your Fun Guy <78428114+The-Imortal-Crasher@users.noreply.github.com>
2021-12-02 13:44:01 -0600

    Update loader.js

--- api/loader.js
+++ api/loader.js
@@ -13,13 +13,13 @@
                   if(img.offsetTop < (window.innerHeight + scrollTop)) {
                     img.src = img.dataset.src;
                     img.classList.remove('lazy');
+                    document.getElementById("imgloader").style.display = "none";
                   }
               });
               if(lazyloadImages.length == 0) { 
                 document.removeEventListener("scroll", lazyload);
                 window.removeEventListener("resize", lazyload);
                 window.removeEventListener("orientationChange", lazyload);
-                document.getElementById("imgloader").style.display = "none";
               }
           }, 20);
         }
